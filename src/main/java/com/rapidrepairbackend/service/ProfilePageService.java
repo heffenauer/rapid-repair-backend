1954f8a39b6779d51bde3e6039c1b3656016be41
Din Becirbasic <dinbecirbasic@gmail.com>
2024-06-01 02:13:39 +0200

    Making Registration Possible. Commented old files because they were simulations of functionalities.

--- src/main/java/com/rapidrepairbackend/service/ProfilePageService.java
+++ src/main/java/com/rapidrepairbackend/service/ProfilePageService.java
@@ -1,70 +1,68 @@
-package com.rapidrepairbackend.service;
-
-
-import com.rapidrepairbackend.entity.Admin;
-import com.rapidrepairbackend.entity.User;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProfilePageService {
-
-    private long id = 2;
-
-    User user = new User(id++, "Ado", "Ikeric", 22, "adi.okeric@stu.ssst.edu.ba", "adi123", "gaserman");
-   Admin admin = new Admin(1, "", "", "");
-
-
-    public User currentUser() {
-        return user;
-
-    }
-
-    public Admin currentAdmin() {
-        return admin;
-    }
-
-    public User updateCurrentUserInformation(User newUser) {
-        User currentUser = this.currentUser();
-        if (newUser.getName() == "" || newUser.getSurname() == "" || newUser.getAge() == 0 || newUser.getEmail() == "" || newUser.getPassword() == "") {
-            System.out.println("Please enter all details");
-        } else {
-
-
-            currentUser.setName(newUser.getName());
-            currentUser.setSurname(newUser.getSurname());
-            currentUser.setAge(newUser.getAge());
-            currentUser.setEmail(newUser.getEmail());
-            currentUser.setPassword(newUser.getPassword());
-            currentUser.setId(this.id++);
-
-        }
-        return currentUser;
-    }
-
-    public void deleteCurrentUserInformation() {
-        user.setAge(0);
-        user.setSurname("");
-        user.setEmail("");
-        user.setPassword("");
-        user.setName("");
-        user.setId(0);
-    }
-
-    public String checkIfAllInformationExists() {
-        if (this.user.getId() == 0 || this.user.getName() == null || this.user.getSurname() == null || this.user.getAge() == 0 || this.user.getEmail() == null || this.user.getPassword() == null) {
-            return "Please enter missing user details.";
-        } else return "All information exists.";
-    }
-
-    public Admin loginAdmin(Admin newAdmin) {
-        if ((newAdmin.getUsername().equals("Admin") || newAdmin.getEmail().equals("admin@admin.com")) && newAdmin.getPassword().equals("admin123")) {
-            deleteCurrentUserInformation();
-            admin = newAdmin;
-            return this.currentAdmin();
-
-        } else {
-            System.out.println("Wrong username/email or password.");
-        }
-        return null;
-    }
-}
+//package com.rapidrepairbackend.service;
+//
+//
+//import com.rapidrepairbackend.entity.Admin;
+//import com.rapidrepairbackend.entity.User;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class ProfilePageService {
+//
+//    private long id = 2;
+//
+//    User user = new User();
+//   Admin admin = new Admin(1, "", "", "");
+//
+//
+//    public User currentUser() {
+//        return user;
+//
+//    }
+//
+//    public Admin currentAdmin() {
+//        return admin;
+//    }
+//
+//    public User updateCurrentUserInformation(User newUser) {
+//        User currentUser = this.currentUser();
+//        if (newUser.getFirstName() == "" || newUser.getLastName() == "" || newUser.getEmail() == "" || newUser.getPassword() == "") {
+//            System.out.println("Please enter all details");
+//        } else {
+//
+//
+//            currentUser.setFirstName(newUser.getFirstName());
+//            currentUser.setLastName(newUser.getLastName());
+//            currentUser.setEmail(newUser.getEmail());
+//            currentUser.setPassword(newUser.getPassword());
+//            currentUser.setId(this.id++);
+//
+//        }
+//        return currentUser;
+//    }
+//
+//    public void deleteCurrentUserInformation() {
+//        user.setLastName("");
+//        user.setEmail("");
+//        user.setPassword("");
+//        user.setFirstName("");
+//        user.setId(0);
+//    }
+//
+//    public String checkIfAllInformationExists() {
+//        if (this.user.getId() == 0 || this.user.getFirstName() == null || this.user.getLastName() == null || this.user.getEmail() == null || this.user.getPassword() == null) {
+//            return "Please enter missing user details.";
+//        } else return "All information exists.";
+//    }
+//
+//    public Admin loginAdmin(Admin newAdmin) {
+//        if ((newAdmin.getUsername().equals("Admin") || newAdmin.getEmail().equals("admin@admin.com")) && newAdmin.getPassword().equals("admin123")) {
+//            deleteCurrentUserInformation();
+//            admin = newAdmin;
+//            return this.currentAdmin();
+//
+//        } else {
+//            System.out.println("Wrong username/email or password.");
+//        }
+//        return null;
+//    }
+//}
